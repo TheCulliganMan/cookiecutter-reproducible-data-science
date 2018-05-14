8b1046ab73f303d7da0c24080800819b46a71a28
DataScience <DataScience@chrobinson.com>
2018-05-14 12:11:45 -0500

    add builds

--- {{cookiecutter.project_slug}}/bin/build-scripts/build-notebook-dockerfile.sh
+++ {{cookiecutter.project_slug}}/bin/build-scripts/build-notebook-dockerfile.sh
@@ -1,4 +1,4 @@
 # build the base image
 bash bin/build-scripts/build-app-dockerfile.sh
 # build the notebook on top of the base image
-docker build -f dockerfiles/dockerfile-application -t {{cookiecutter.project_slug}}_notebook .
+docker build -f dockerfiles/dockerfile-notebook -t {{cookiecutter.project_slug}}-notebook .

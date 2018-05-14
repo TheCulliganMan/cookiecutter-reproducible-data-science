2e4243a0317ab3c65f4bfe9ccfcb20616aa5c7a9
DataScience <DataScience@chrobinson.com>
2018-05-14 12:02:44 -0500

    update dockerfiles

--- {{cookiecutter.project_slug}}/bin/build-scripts/build-app-dockerfile.sh
+++ {{cookiecutter.project_slug}}/bin/build-scripts/build-app-dockerfile.sh
@@ -1,2 +1,2 @@
 # build the base image
-docker build -f dockerfiles/dockerfile-application -t {{cookiecutter.project_slug}}_application ./src
+docker build -f dockerfiles/dockerfile-application -t {{cookiecutter.project_slug}}_application .
--- {{cookiecutter.project_slug}}/bin/build-scripts/build-notebook-dockerfile.sh
+++ {{cookiecutter.project_slug}}/bin/build-scripts/build-notebook-dockerfile.sh
@@ -1,4 +1,4 @@
 # build the base image
 bash bin/build-scripts/build-app-dockerfile.sh
 # build the notebook on top of the base image
-docker build -f dockerfiles/dockerfile-application -t {{cookiecutter.project_slug}}_notebook ./notebooks
+docker build -f dockerfiles/dockerfile-application -t {{cookiecutter.project_slug}}_notebook .

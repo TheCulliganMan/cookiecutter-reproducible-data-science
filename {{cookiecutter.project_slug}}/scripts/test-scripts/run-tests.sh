d771ce267c7f374c34b679f97d52bd81ef86d502
DataScience <DataScience@chrobinson.com>
2018-05-17 08:36:41 -0500

    change copy

--- {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
+++ {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
@@ -1,1 +1,10 @@
-python -m pytest --cov=./src ./tests
+# Build the application dockerfile
+bash scripts/build-scripts/build-app-dockerfile.sh
+# Run the tests inside a docker container
+docker run \
+    -v ./config:/usr/src/app/config \
+    -v ./data:/usr/src/app/data \
+    -v ./reports:/usr/src/app/reports \
+    -v ./scripts:/usr/src/app/scripts \
+    {{cookiecutter.project_slug}}-application \
+    python -m pytest --cov=./src ./tests

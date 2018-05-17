32fcbdfb4e20abaa33dc28a4c2becfb32ef25332
DataScience <DataScience@chrobinson.com>
2018-05-17 09:03:34 -0500

    working tests

--- {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
+++ {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
@@ -1,10 +1,11 @@
 # Build the application dockerfile
 bash scripts/build-scripts/build-app-dockerfile.sh
+
 # Run the tests inside a docker container
-docker run \
-    -v ./config:/usr/src/app/config \
-    -v ./data:/usr/src/app/data \
-    -v ./reports:/usr/src/app/reports \
-    -v ./scripts:/usr/src/app/scripts \
+docker run -it \
+    -v $(pwd)/config/:/usr/src/app/config \
+    -v $(pwd)/data/:/usr/src/app/data \
+    -v $(pwd)/reports/:/usr/src/app/reports \
+    -v $(pwd)/scripts/:/usr/src/app/scripts \
     {{cookiecutter.project_slug}}-application \
-    python -m pytest --cov=./src ./tests
+    python -m pytest --cov=src

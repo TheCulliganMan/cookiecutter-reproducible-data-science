0920a4039718fd8255a0979fbeeb22e68d7396a3
Ryan Culligan <ryan.culligan@chrobinson.com>
2018-09-17 15:47:05 -0500

    move tests

--- {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
+++ {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
@@ -1,11 +1,5 @@
 # Build the application dockerfile
-bash scripts/build-scripts/build-app-dockerfile.sh
+# bash scripts/build-scripts/build-app-dockerfile.sh
 
 # Run the tests inside a docker container
-docker run -it \
-    -v $(pwd)/config/:/usr/src/app/config \
-    -v $(pwd)/data/:/usr/src/app/data \
-    -v $(pwd)/reports/:/usr/src/app/reports \
-    -v $(pwd)/scripts/:/usr/src/app/scripts \
-    {{cookiecutter.project_slug}}-application \
-    python -m pytest --cov=src
+docker-compose -f docker-compose-development.yml exec anheuserbuschbidder-application pytest --cov --cov-config=.coveragerc

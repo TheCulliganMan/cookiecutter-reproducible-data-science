0c6bf466f25a07b6e8b361a98edb429b9d6625f8
Ryan Culligan <ryan.culligan@chrobinson.com>
2018-10-15 08:15:02 -0500

    add makefile

--- {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
+++ {{cookiecutter.project_slug}}/scripts/test-scripts/run-tests.sh
@@ -1,5 +1,5 @@
 # Build the application dockerfile
-# bash scripts/build-scripts/build-app-dockerfile.sh
+bash scripts/build-scripts/build-app-dockerfile.sh
 
 # Run the tests inside a docker container
 docker-compose -f docker-compose-development.yml exec anheuserbuschbidder-application pytest --cov --cov-config=.coveragerc
